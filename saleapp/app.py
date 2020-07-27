dfb78f090b8f7ca42f2d67dd84a7982aa8221d01
hoangduy2910 <hoangduy29101999@gmail.com>
2020-07-27 11:11:45 +0700

    Update BTTH1

--- saleapp/app.py
+++ saleapp/app.py
@@ -1,4 +1,4 @@
-from flask import render_template,request
+from flask import render_template, request, redirect, url_for
 from saleapp import app
 from saleapp import dal
 
@@ -9,8 +9,6 @@ def index():
 @app.route("/products")
 def products():
     keyword = request.args["keyword"] if request.args.get("keyword") else None
-    #import pdb
-    #pdb.set_trace()
     return render_template("products.html", products=dal.read_products(keyword=keyword))
 
 @app.route("/products/<int:category_id>")
@@ -19,9 +17,36 @@ def products_by_category_id(category_id):
 
 @app.route("/products/add", methods=["post", "get"])
 def add_product():
+    error_msg = None
+    categories = dal.read_categories()
+
     if request.method == "POST":
-        print("123")
-    return render_template("add-product.html")
+        if dal.add_product(**dict(request.form)):
+            return redirect(url_for('products'))
+        else:
+            error_msg = "Không thêm được sản phẩm. Vui lòng thử lại"
+
+    return render_template("add-product.html", categories=categories, error_msg=error_msg)
+
+@app.route("/products/update/<int:product_id>", methods=["post", "get"])
+def update_product(product_id):
+    error_msg = None
+    product = dal.read_product_by_id(product_id)
+    categories = dal.read_categories()
+
+    if request.method == "POST":
+        d = dict(request.form.copy())
+        d["product_id"] = product_id
+        if dal.update_product(**d):
+            return redirect(url_for('products'))
+        else:
+            error_msg = "Không cập nhật được sản phẩm. Vui lòng thử lại"
+
+    return render_template("update-product.html", product=product[0], categories=categories, error_msg=error_msg)
+
+@app.route("/products/delete/<int:product_id>", methods=["post", "get"])
+def delete_product(product_id):
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
--- saleapp/dal.py
+++ saleapp/dal.py
@@ -17,3 +17,43 @@ def read_products(keyword=None, from_price=None, to_price=None):
 
 def read_products_by_category_id(category_id):
     return [product for product in read_products() if product["category_id"] == category_id]
+
+def read_product_by_id(product_id):
+    return [product for product in read_products() if product["id"] == product_id]
+
+def add_product(name, description, price, image, category_id):
+    products = read_products()
+    products.append({
+        "id": len(products) + 1,
+        "name": name,
+        "description": description,
+        "price": int(price),
+        "image": image,
+        "category_id": int(category_id)
+    })
+    return update_product_json(products)
+
+def update_product(product_id, name, description, price, image, category_id):
+    products = read_products()
+    for product in products:
+        if product["id"] == product_id:
+            product["name"] = name
+            product["description"] = description
+            product["price"] = int(price)
+            product["image"] = image
+            product["category_id"] = int(category_id)
+            break
+    return update_product_json(products)
+
+def update_product_json(products):
+    try:
+        with open(os.path.join(app.root_path, 'data/products.json'), "w", encoding="utf-8") as f:
+            json.dump(products, f, ensure_ascii=False, indent=4)
+        return True
+    except Exception as ex:
+        return False
+
+
+def read_categories():
+    with open(os.path.join(app.root_path, 'data/categories.json'), encoding="utf-8") as f:
+        return json.load(f)
